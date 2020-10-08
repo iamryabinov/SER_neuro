d1db63b98c3d5d47685f2452eb11cf1bf77cc90e
iamryabinov <iamryabinov@gmail.com>
2020-10-08 23:44:04 +0300

    wip

--- constants.py
+++ constants.py
@@ -2,11 +2,11 @@
 # IEMOCAP_PATH_TO_WAVS = 'datasets'
 # IEMOCAP_PATH_TO_WAVS = 'datasets\\'
 
-IEMOCAP_PATH_TO_WAVS = 'E:\\Projects\\SER\\datasets\\IEMOCAP_full_release\\iemocap_audios\\'
-IEMOCAP_PATH_FOR_PARSER = 'E:\\Projects\\SER\\datasets\\IEMOCAP_full_release\\'
-PATH_TO_PICKLE = 'datasets\\'
-IEMOCAP_PATH_TO_EGEMAPS = 'E:\\Projects\\SER\\datasets\\IEMOCAP_full_release\\iemocap_audios\\features\\egemaps.csv'
-RAMAS_PATH_TO_WAVS = 'E:\\Projects\\SER\\datasets\\RAMAS\\Audio\\Audio_cut\\'
+# IEMOCAP_PATH_TO_WAVS = 'E:\\Projects\\SER\\datasets\\IEMOCAP_full_release\\iemocap_audios\\'
+# IEMOCAP_PATH_FOR_PARSER = 'E:\\Projects\\SER\\datasets\\IEMOCAP_full_release\\'
+# PATH_TO_PICKLE = 'datasets\\'
+# IEMOCAP_PATH_TO_EGEMAPS = 'E:\\Projects\\SER\\datasets\\IEMOCAP_full_release\\iemocap_audios\\features\\egemaps.csv'
+# RAMAS_PATH_TO_WAVS = 'E:\\Projects\\SER\\datasets\\RAMAS\\Audio\\Audio_cut\\'
 
 # RANDOM_SEED = 42
 # WEIGHTS_FOLDER = 'models\\training sessions\\'
@@ -14,10 +14,10 @@ RAMAS_PATH_TO_WAVS = 'E:\\Projects\\SER\\datasets\\RAMAS\\Audio\\Audio_cut\\'
 import os
 
 
-# IEMOCAP_PATH_TO_WAVS = ''
-# IEMOCAP_PATH_FOR_PARSER = ''
-# PATH_TO_PICKLE = 'datasets/'
-# IEMOCAP_PATH_TO_EGEMAPS = ''
+IEMOCAP_PATH_TO_WAVS = ''
+IEMOCAP_PATH_FOR_PARSER = ''
+PATH_TO_PICKLE = 'datasets/'
+IEMOCAP_PATH_TO_EGEMAPS = ''
 
 
 RANDOM_SEED = 42
